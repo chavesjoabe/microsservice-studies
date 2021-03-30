d3d17aaac36d5e9ccff8acadc38dbb81431c0b23
chavesjoabe <chavesjoabe1@gmail.com>
2021-03-29 23:57:29 -0300

    hystrix fault tolerance

--- hr-payroll2/src/main/java/com/joabechaves/hrpayroll2/controllers/PaymentController.java
+++ hr-payroll2/src/main/java/com/joabechaves/hrpayroll2/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.joabechaves.hrpayroll2.controllers;
 
 import com.joabechaves.hrpayroll2.entities.Payment;
 import com.joabechaves.hrpayroll2.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +18,15 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         return ResponseEntity.status(HttpStatus.OK).body(this.service.getPayment(workerId, days));
     }
-}
+
+    public ResponseEntity<Payment> getPaymentAlternative( Long workerId,  Integer days){
+        Payment payment = new Payment("José das Couves", 500.0, 12);
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    }
--- hr-worker/src/main/java/com/joabechaves/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/joabechaves/hrworker/controllers/WorkerController.java
@@ -34,7 +34,16 @@ public class WorkerController {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
 
+        /*
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         */
+
         logger.info("port = "+ env.getProperty("local.server.port"));
+
         return ResponseEntity.status(HttpStatus.OK).body(this.service.findById(id));
     }
 }
