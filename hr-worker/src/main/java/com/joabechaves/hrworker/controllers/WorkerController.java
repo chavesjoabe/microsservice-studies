caf77280bca4b15a9e878764221ae1079e085f7b
chavesjoabe <chavesjoabe1@gmail.com>
2021-03-30 12:52:46 -0300

    actuator for runtime config update

--- hr-worker/src/main/java/com/joabechaves/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/joabechaves/hrworker/controllers/WorkerController.java
@@ -5,6 +5,8 @@ import com.joabechaves.hrworker.services.WorkerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerService service;
+
+    @GetMapping("/configs")
+    public ResponseEntity<Void> getConfig(){
+        logger.info("Config = "+testConfig);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
